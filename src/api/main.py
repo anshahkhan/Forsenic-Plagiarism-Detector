b5f9c3cdc1d22bf672cf185cde7cc0ac21af08ff
Anshah Khan <shahjeez1217@gmail.com>
2025-11-24 16:42:26 +0500

    Merged New Json Work with Pipeline API

--- src/api/main.py
+++ src/api/main.py
@@ -3,12 +3,9 @@ from fastapi import FastAPI
 from src.api.ingestion_api import router as ingestion_router
 from src.api.similarity_api import router as similarity_router
 from src.api.forsenics_api import router as forsenics_router
-<<<<<<< HEAD
 from src.api.pipeline_api import router as pipeline_router
 
-=======
 from src.api.newjson import router as Clean_router
->>>>>>> 20b6e8ab029549b1c5c3a512772d531f8ea99a13
 app = FastAPI(title="DF Project - MVP")
 
 # Include ingestion routes
@@ -19,12 +16,9 @@ app.include_router(similarity_router)
 
 app.include_router(forsenics_router)
 
-<<<<<<< HEAD
 
 app.include_router(pipeline_router)
-=======
 app.include_router(Clean_router)
->>>>>>> 20b6e8ab029549b1c5c3a512772d531f8ea99a13
 
 @app.get("/")
 def read_root():
